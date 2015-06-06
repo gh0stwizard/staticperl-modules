9aa86032f1506503e8b3ded8c66631174236ca33
gh0stwizard <vitaliy.tokarev@gmail.com>
2015-06-06 14:40:06 +0300

    backup-staticperl.sh creates file listing

--- misc/backup-staticperl.sh
+++ misc/backup-staticperl.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 # path where backups will be kept
-BACKUP_DIR="$HOME/tmp/backup"
+BACKUP_DIR="$HOME/backup/SP"
 
 . $HOME/.staticperlrc
 
-PERL_DIR=".staticperl-$PERL_VERSION"
+PERL_DIR=$(basename ${STATICPERL})
 BACKUP_FILE="${BACKUP_DIR}/staticperl-$PERL_VERSION-$(date +%s).tar.gz"
+FILES_LIST="${BACKUP_DIR}/staticperl-$PERL_VERSION-$(date +%s).filelist.txt"
 
 tar -zcvpf ${BACKUP_FILE} \
     --directory=$HOME \
@@ -14,9 +15,10 @@ tar -zcvpf ${BACKUP_FILE} \
     --exclude="${PERL_DIR}/cpan/build/*" \
     --exclude="${PERL_DIR}/cpan/sources/*" \
     --exclude="${PERL_DIR}/cache/*" \
-    $PERL_DIR \
+    $PERL_DIR > ${FILES_LIST} \
 || exit 1
 
 echo ">>> ${BACKUP_FILE}"
+echo ">>> ${FILES_LIST}"
 
 exit 0
